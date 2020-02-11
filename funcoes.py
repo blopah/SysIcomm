a450126a0b912e585f4549c09d1101d3bad708ed
Pablo Lucena <pablodplucenahh@gmail.com>
2020-02-10 21:19:41 -0300

    Arquivos adicionados

---
+++ funcoes.py
@@ -0,0 +1,103 @@
+import os
+import shutil
+from datetime import date
+
+def copiaRefs(referencia):
+
+    hoje = date.today().strftime("%Y-%m-%d")
+    today = f'Data - {hoje}'
+
+    prefixOriS2G = 'C:\\Users\\Pablo\\Documents\\SandBox\\Sandbox Sys Icomm\\Servidor\\S2G\\Originais'.split('\\')
+    prefixOriOQV = 'C:\\Users\\Pablo\\Documents\\SandBox\\Sandbox Sys Icomm\\Servidor\\Icommgroup\\OQV\\Originais'.split('\\')
+    prefixUser = 'C:\\Users\\Pablo\\Documents\\SandBox\\Sandbox Sys Icomm\\Usuario\\Pablo Lucena'.split('\\')
+    # print(prefixUser)
+    prefixUserS2G = prefixUser + [today] + ['S2G']
+    prefixUserOQV = prefixUser + [today] + ['OQV']
+    # print(prefixUserS2G)
+    # print(prefixUserOQV)
+    try:
+        for refs in referencia:
+            if refs[0] == 'S2G':
+                del refs[0]
+                srcPath = '\\'.join(prefixOriS2G + refs)
+                dstPath = '\\'.join(prefixUserS2G + refs)
+                shutil.copytree(srcPath, dstPath)
+                # print(f' dstPath {dstPath}')
+                # print(f' srcPath {srcPath}')
+            elif refs[0] == 'OQV':
+                del refs[0]
+                srcPath = '\\'.join(prefixOriOQV + refs)
+                dstPath = '\\'.join(prefixUserOQV + refs)
+                shutil.copytree(srcPath, dstPath)
+                # print(f' dstPath {dstPath}')
+                # print(f' srcPath {srcPath}')
+            else:
+                print('Nao foi possível interpretar o caminho inserido.')
+                return 'Nao foi possível interpretar o caminho inserido.'
+        return 'Tarefa Finalizada'
+    except FileNotFoundError:
+        print('Caminho não encontrado no servidor')
+        return 'Caminho não encontrado no servidor'
+    except FileExistsError:
+        print('Caminho ja existente no seu Mac')
+        return 'Caminho ja existente no seu Mac'
+
+
+def criaCaminho(referencia):
+    if referencia == 'IndexError':
+        return 'Nao foi possível interpretar o caminho inserido.'
+    # Pega a data de hoje
+    hoje = date.today().strftime("%Y-%m-%d")
+    today = f'Data - {hoje}'
+    # print(today)
+    # Converte a lista do caminho em um caminho
+    referencias = list()
+    for refes in referencia:
+        refs = '\\'.join(refes[:-1])
+        referencias.append(refs)
+    # print(referencias)
+    # Defino o prefixo do caminho do User
+    prefixUser = 'C:\\Users\\Pablo\\Documents\\SandBox\\Sandbox Sys Icomm\\Usuario\\Pablo Lucena'.split('\\')
+    # Junta para o caminho do User, e Cria os caminhos
+    for refs in referencias:
+        caminho = os.path.join('\\'.join(prefixUser), today, refs)
+        # print(caminho)
+        if not os.path.exists(caminho):
+            os.makedirs(caminho)
+
+
+def listaCaminho(secao):
+#     secao = '''INVERNO 20	HAIGHT	1416	01030006_VERDECACT	MODELO E STILL	S2G
+# INVERNO 20	HAIGHT	1416	01040006_VERDECACT	MODELO E STILL	S2G'''
+    # print(secao)
+    secaolist = secao.split('\n')
+    # print(secaolist)
+    secaolistref = list()
+    for refe in secaolist:
+        ref = refe.split('\t')  #Isso torna a linha, uma lista
+        secaolistref.append(ref)  #Isso appends a lista que foi criada inteira.
+        # print(secaolistref)
+    # print('-' * 40)
+    # print(secaolistref)
+    referencias = list()
+    try:
+        for ref in secaolistref:
+            ref.insert(0, ref[5])
+            # print(f'{ref[:5]}')
+            referencias.append(ref[:5])
+        # print('-' * 40)
+        print(referencias)
+        return referencias
+    except IndexError:
+        print('IndexError')
+    return 'IndexError'
+
+
+# criaPastas()
+# copiaPath()
+# for ref in listaCaminho():
+#     print(ref)
+# criaCaminho(listaCaminho())
+# copiaRefs(listaCaminho())
+# mapeiaPastas()
+# Teste
---
+++ programa.pyw
@@ -0,0 +1,51 @@
+import funcoes
+from tkinter import *
+
+
+def take():
+    feedback['text'] = 'Pegando...'
+    secao = text_area.get()
+    funcoes.criaCaminho(funcoes.listaCaminho(secao))
+    feedback['text'] = funcoes.copiaRefs(funcoes.listaCaminho(secao))
+    # feedback['text'] = 'Tarefa finalizada'
+
+
+def drop():
+    feedback['text'] = 'Droping!'
+
+
+janela = Tk()
+janela.geometry('530x110')
+janela.title('Gerenciador de Pastas')
+janela.wm_iconbitmap('favicon.ico')
+
+container1 = Frame(janela)
+container1.pack()
+
+container2 = Frame(janela)
+container2.pack()
+
+container3 = Frame(janela, borderwidth=5)
+container3.pack()
+
+container4 = Frame(janela)
+container4.pack()
+
+enunciated = Label(container1, text='Cole os caminhos', font=('Arial', '11', 'bold'), fg='#353535')
+enunciated.pack()
+
+text_area = Entry(container2, width='70', bg='#353535', fg='#f2f2f2', font=('Arial', '9', 'bold'))
+text_area.pack()
+
+take_button = Button(container3, text='Pegar', font=('Arial', '9', 'bold'), border=3, padx=102, command=take,
+                     fg='#353535')
+take_button.pack(side=LEFT)
+
+drop_button = Button(container3, text='Jogar', font=('Arial', '9', 'bold'), border=3, padx=102, command=drop,
+                     fg='#353535')
+drop_button.pack(side=LEFT)
+
+feedback = Label(container4, text='Testando a Fonte', font=('Arial', '11', 'bold'), fg='#353535')
+feedback.pack()
+
+janela.mainloop()
